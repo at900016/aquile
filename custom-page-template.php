aadfdca2b4221acfa99ab18fc1faf31cd00268fc
Ajit <at900016@gmail.com>
2021-07-08 08:36:47 +0530

    hjghj

--- custom-page-template.php
+++ custom-page-template.php
@@ -1,66 +1,96 @@
 <?php
-
 /**
-* Template Name: Sumit
+* Template Name: Custum_template
 *
 */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
 
-// get_headers();
+	<?php
 
-$args = array(
-  'numberposts' => -1,
-  'post_type'   => 'book'
-);
+	$args = array(
+		'numberposts' => -1,
+		'post_type'   => 'book'
+	);
 
-$query = new WP_Query( array(
+	$query = new WP_Query( array(
+		's' => $_GET['search_item'],
+		'post_type'             => 'book',
+		'posts_per_page'        => -1,
+	));
 
-    'post_type'             => 'book',
-    'posts_per_page'        => -1,
-    'tax_query' => array(
-        'relation' => 'OR',
-        array(
-            'taxonomy' => 'Language',
-            'field'    => 'name',
-            'terms'    => $_REQUEST['lang'],
-        ),
-        array(
-            'taxonomy' => 'Language',
-            'field'    => 'name',
-            'terms'    => 'English',
-        ),
-        // array(
-        //     'taxonomy' => 'property_city',
-        //     'field'    => 'id',
-        //     'terms'    => $property_city_terms,
-        // ),
-        // array(
-        //     'taxonomy' => 'property_state',
-        //     'field'    => 'id',
-        //     'terms'    => $property_state_terms,
-        // ),
-        // array(
-        //     'taxonomy' => 'property_feature',
-        //     'field'    => 'id',
-        //     'terms'    => $property_feature_terms,
-        // ),
-    ),
-
-));
- 
-echo "<pre>";
-// print_r($query);
-
-$cut_tax = array('Language','writer');
-foreach ($cut_tax as $key => $value1) {
-	$terms = get_terms( array(
-	    'taxonomy' => $value1,
-	    'hide_empty' => false,
-	) );
 	?>
-	
-	<?php foreach ($terms as $key => $value): ?>
-		<input type="checkbox" name="<?= $value1 ?>[]" value="<?= $value->name ?>"> <?= $value->name ?>	
-	<?php endforeach ?>
-<?php } ?>
 
-<?php //get_footer(); ?>
+
+	<style type="text/css">
+	.card {
+		/* Add shadows to create the "card" effect */
+		box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%);
+		transition: 0.3s;
+		width: 30%;
+		float: left;
+		margin: 0 1% 1% 0;
+
+	}
+
+	/* On mouse-over, add a deeper shadow */
+	.card:hover {
+		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+	}
+
+	/* Add some padding inside the card container */
+	.container {
+		padding: 2px 16px;
+	}
+</style>
+
+<form>
+<div class="section">
+	<div class="search_bar">
+		<input type="text" name="search_item" value="<?= $_REQUEST['search_item'] ?>">
+		<input type="submit" value="Search">
+	</div>
+</div>
+<div class="section" style="width: 100%;">
+	<div class="one_forth" style="width:30%; float: left;">
+		<?php
+		$cut_tax = array('author','subject', 'language', 'exam');
+		foreach ($cut_tax as $key => $value1) {
+
+			$terms = get_terms( array(
+				'taxonomy' => $value1,
+				'hide_empty' => false,
+			) );
+			?>
+			<h1><?= $value1 ?> </h1>
+
+			<?php foreach ($terms as $key => $value): ?>
+				<input type="checkbox" name="<?= $value1 ?>[]" value="<?= $value->name ?>"> <?= $value->name ?>	<br/>
+			<?php endforeach ?>
+		<?php } ?>
+	</div>
+	<div class="three_fourth" style="width:68%;float: left;">
+		<?php 
+		echo "<pre>";
+		print_r($query);
+		echo "</pre>";
+		?>
+		<?php foreach ($query->posts as $key => $value) {?>
+			<div class="card post_id_<?= $value->ID ?>">
+				<img src="<?= get_the_post_thumbnail_url($value->ID) ?>" alt="Avatar" style="width:100%">
+				<div class="container">
+					<h4><b><?= $value->post_title ?></b></h4>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+</div>
+</form>
+</body>
+</html>
--- taxonomy_custom.php
+++ taxonomy_custom.php
@@ -128,22 +128,22 @@ function wpdocs_create_book_taxonomies() {
  
     // Add new taxonomy, NOT hierarchical (like tags)
     $labels = array(
-        'name'                       => _x( 'Writers', 'taxonomy general name', 'textdomain' ),
-        'singular_name'              => _x( 'Writer', 'taxonomy singular name', 'textdomain' ),
-        'search_items'               => __( 'Search Writers', 'textdomain' ),
-        'popular_items'              => __( 'Popular Writers', 'textdomain' ),
-        'all_items'                  => __( 'All Writers', 'textdomain' ),
+        'name'                       => _x( 'Authors', 'taxonomy general name', 'textdomain' ),
+        'singular_name'              => _x( 'Author', 'taxonomy singular name', 'textdomain' ),
+        'search_items'               => __( 'Search Authors', 'textdomain' ),
+        'popular_items'              => __( 'Popular Authors', 'textdomain' ),
+        'all_items'                  => __( 'All Authors', 'textdomain' ),
         'parent_item'                => null,
         'parent_item_colon'          => null,
-        'edit_item'                  => __( 'Edit Writer', 'textdomain' ),
-        'update_item'                => __( 'Update Writer', 'textdomain' ),
-        'add_new_item'               => __( 'Add New Writer', 'textdomain' ),
-        'new_item_name'              => __( 'New Writer Name', 'textdomain' ),
-        'separate_items_with_commas' => __( 'Separate writers with commas', 'textdomain' ),
-        'add_or_remove_items'        => __( 'Add or remove writers', 'textdomain' ),
-        'choose_from_most_used'      => __( 'Choose from the most used writers', 'textdomain' ),
-        'not_found'                  => __( 'No writers found.', 'textdomain' ),
-        'menu_name'                  => __( 'Writers', 'textdomain' ),
+        'edit_item'                  => __( 'Edit Author', 'textdomain' ),
+        'update_item'                => __( 'Update Author', 'textdomain' ),
+        'add_new_item'               => __( 'Add New Author', 'textdomain' ),
+        'new_item_name'              => __( 'New Author Name', 'textdomain' ),
+        'separate_items_with_commas' => __( 'Separate Authors with commas', 'textdomain' ),
+        'add_or_remove_items'        => __( 'Add or remove Authors', 'textdomain' ),
+        'choose_from_most_used'      => __( 'Choose from the most used Authors', 'textdomain' ),
+        'not_found'                  => __( 'No Authors found.', 'textdomain' ),
+        'menu_name'                  => __( 'Authors', 'textdomain' ),
     );
  
     $args = array(
@@ -153,10 +153,76 @@ function wpdocs_create_book_taxonomies() {
         'show_admin_column'     => true,
         'update_count_callback' => '_update_post_term_count',
         'query_var'             => true,
-        'rewrite'               => array( 'slug' => 'writer' ),
+        'rewrite'               => array( 'slug' => 'Author' ),
     );
  
-    register_taxonomy( 'writer', 'book', $args );
+    register_taxonomy( 'author', 'book', $args );
+
+
+    $labels = array(
+        'name'                       => _x( 'Subjects', 'taxonomy general name', 'textdomain' ),
+        'singular_name'              => _x( 'Subject', 'taxonomy singular name', 'textdomain' ),
+        'search_items'               => __( 'Search Subjects', 'textdomain' ),
+        'popular_items'              => __( 'Popular Subjects', 'textdomain' ),
+        'all_items'                  => __( 'All Subjects', 'textdomain' ),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __( 'Edit Subject', 'textdomain' ),
+        'update_item'                => __( 'Update Subject', 'textdomain' ),
+        'add_new_item'               => __( 'Add New Subject', 'textdomain' ),
+        'new_item_name'              => __( 'New Subject Name', 'textdomain' ),
+        'separate_items_with_commas' => __( 'Separate Subjects with commas', 'textdomain' ),
+        'add_or_remove_items'        => __( 'Add or remove Subjects', 'textdomain' ),
+        'choose_from_most_used'      => __( 'Choose from the most used Subjects', 'textdomain' ),
+        'not_found'                  => __( 'No Subjects found.', 'textdomain' ),
+        'menu_name'                  => __( 'Subjects', 'textdomain' ),
+    );
+ 
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'Subject' ),
+    );
+ 
+    register_taxonomy( 'subject', 'book', $args );
+
+
+
+
+    $labels = array(
+        'name'                       => _x( 'Exams', 'taxonomy general name', 'textdomain' ),
+        'singular_name'              => _x( 'Exam', 'taxonomy singular name', 'textdomain' ),
+        'search_items'               => __( 'Search Exams', 'textdomain' ),
+        'popular_items'              => __( 'Popular Exams', 'textdomain' ),
+        'all_items'                  => __( 'All Exams', 'textdomain' ),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __( 'Edit Exam', 'textdomain' ),
+        'update_item'                => __( 'Update Exam', 'textdomain' ),
+        'add_new_item'               => __( 'Add New Exam', 'textdomain' ),
+        'new_item_name'              => __( 'New Exam Name', 'textdomain' ),
+        'separate_items_with_commas' => __( 'Separate Exams with commas', 'textdomain' ),
+        'add_or_remove_items'        => __( 'Add or remove Exams', 'textdomain' ),
+        'choose_from_most_used'      => __( 'Choose from the most used Exams', 'textdomain' ),
+        'not_found'                  => __( 'No Exams found.', 'textdomain' ),
+        'menu_name'                  => __( 'Exams', 'textdomain' ),
+    );
+ 
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'Exam' ),
+    );
+ 
+    register_taxonomy( 'exam', 'book', $args );
 }
 // hook into the init action and call create_book_taxonomies when it fires
 add_action( 'init', 'wpdocs_create_book_taxonomies', 0 );
@@ -165,8 +231,55 @@ add_action( 'init', 'wpdocs_create_book_taxonomies', 0 );
 add_filter( 'page_template', 'wpa3396_page_template' );
 function wpa3396_page_template( $page_template )
 {
-    if ( is_page( 'search-page' ) ) {
+    if ( is_page( 'sample-page' ) ) {
         $page_template = dirname( __FILE__ ) . '/custom-page-template.php';
     }
     return $page_template;
 }
+
+
+
+/**
+ * Register meta box(es).
+ */
+function bookmeta_register_meta_boxes() {
+	add_meta_box( 'home_page_setting_meta_box', __( 'Meta for texonomy', 'textdomain' ), 'book_post_metabox_html', 'book' );
+}
+add_action( 'add_meta_boxes', 'bookmeta_register_meta_boxes' );
+
+/**
+ * Meta box display callback.
+ *
+ * @param WP_Post $post Current post object.
+ */
+function book_post_metabox_html( $post ) {
+	$number_of_page = get_post_meta($post->ID, 'number_of_page', true);
+	$price = get_post_meta($post->ID, 'price', true);
+	$printing_year = get_post_meta($post->ID, 'printing_year', true);
+	$gsm = get_post_meta($post->ID, 'gsm', true);
+	$discount_price = get_post_meta($post->ID, 'discount_price', true);
+	?>
+	Number of Page :: <input type="name" name="number_of_page" value="<?= $number_of_page ?>" > <br>
+	Price :: <input type="text" name="price" value="<?= $price ?>"><br>
+	Printing year :: <input type="text" name="printing_year" value="<?= $printing_year ?>"><br>
+	GSM :: <input type="text" name="gsm" value="<?= $gsm ?>"><br>
+	DISCOUNT Price :: <input type="text" name="discount_price" value="<?= $discount_price ?>"><br>
+	<input type="hidden" name="hps0_form">
+	<?php
+}
+
+/**
+ * Save meta box content.
+ *
+ * @param int $post_id Post ID
+ */
+function bookmeta_save_meta_box( $post_id ) {
+	if(isset($_POST['hps0_form'])){
+		update_post_meta($post_id, 'number_of_page', $_POST['number_of_page']);
+		update_post_meta($post_id, 'price', $_POST['price']);
+		update_post_meta($post_id, 'printing_year', $_POST['printing_year']);
+		update_post_meta($post_id, 'gsm', $_POST['gsm']);
+		update_post_meta($post_id, 'discount_price', $_POST['discount_price']);
+	}
+}
+add_action( 'save_post', 'bookmeta_save_meta_box' );
